d1bd12ac905f8a84d0f46bf05244326f393cbac7
A. Emre Ünal <aemreunal@gmail.com>
2015-05-12 23:43:48 +0300

    Initial Commit

---
+++ OzUNavigation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OzUNavigation
+//
+//  Created by A. Emre Ünal on 12/05/15.
+//  Copyright (c) 2015 A. Emre Ünal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OzUNavigation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OzUNavigation
+//
+//  Created by A. Emre Ünal on 12/05/15.
+//  Copyright (c) 2015 A. Emre Ünal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
