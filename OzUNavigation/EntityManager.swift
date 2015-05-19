efd35bab73bc9e02c7405e0b51f3c3f6da8efd0e
A. Emre Ünal <aemreunal@gmail.com>
2015-05-19 03:50:15 +0300

    Get beacon between a region and connection

--- OzUNavigation/EntityManager.swift
+++ OzUNavigation/EntityManager.swift
@@ -16,43 +16,43 @@ import AFNetworking
 public class EntityManager {
     // Singleton variable
     private static var instance: EntityManager!
-    
+
     // Entity objects
     private(set) var regions = [Int:Region]() // Map Region ID to Region
     private(set) var beacons = [Int:Beacon]() // Map Beacon ID to Beacon
     private(set) var connections = [Int:Connection]() // Map Connection ID to Connection
-    
+
     private(set) var beaconsToRegions = [Int:Int]() // Map Beacon ID to Region ID
     private(set) var beaconsToConnections = [Int:[Connection]]() // Map Beacon ID to Connection
-    
+
     // AFNetworking Manager
     let requestManager: AFHTTPRequestOperationManager
-    
+
     // Server Data Manager
     let serverManager = ServerDataManager.sharedInstance()
-    
+
     // Request operations
     private var regionRequestOperation:AFHTTPRequestOperation!
     private var beaconRequestOperations = [AFHTTPRequestOperation]()
     private var connectionRequestOperation:AFHTTPRequestOperation!
     private var beaconRequestOperationsComplete = 0
-    
+
     // Load view
     private var loadScreenController:LoadViewProtocol!
-    
+
     // Entity load status
     private var entitiesAreLoaded = false
-    
+
     class func sharedInstance() -> EntityManager {
         self.instance = (self.instance ?? EntityManager())
         return self.instance
     }
-    
+
     init() {
         self.requestManager = AFHTTPRequestOperationManager()
         requestManager.securityPolicy.allowInvalidCertificates = true
     }
-    
+
     public func loadEntities(loadView:LoadViewProtocol) {
         self.loadScreenController = loadView
         if entitiesAreLoaded {
@@ -63,7 +63,7 @@ public class EntityManager {
             self.regionRequestOperation.start()
         }
     }
-    
+
     private func createRegionRequest() {
         let jsonUrlRequest = getJsonUrlRequest(url: serverManager.getRegionUrl())
         let request:AFHTTPRequestOperation = requestManager.HTTPRequestOperationWithRequest(jsonUrlRequest,
@@ -72,7 +72,7 @@ public class EntityManager {
         self.regionRequestOperation = request
         println("Region request job created.")
     }
-    
+
     private func createBeaconRequests() {
         for (regionId:Int, _) in regions {
             let jsonUrlRequest = getJsonUrlRequest(url: serverManager.getBeaconUrl(regionId))
@@ -87,10 +87,10 @@ public class EntityManager {
         }
         println("All beacon request jobs created.")
     }
-    
+
     private func createConnectionRequest() {
         let jsonUrlRequest = getJsonUrlRequest(url: serverManager.getConnectionUrl())
-        
+
         let request:AFHTTPRequestOperation = requestManager.HTTPRequestOperationWithRequest(jsonUrlRequest,
             success: handleConnectionRequestSuccess,
             failure: handleRequestFailure)
@@ -100,14 +100,14 @@ public class EntityManager {
         self.connectionRequestOperation = request
         println("Connection request job created.")
     }
-    
+
     private func startBeaconRequests() {
         for request in beaconRequestOperations {
             request.start()
         }
         println("Beacon request jobs started.")
     }
-    
+
     private func handleRegionRequestSuccess(operation: AFHTTPRequestOperation!, responseObject: AnyObject!) -> Void {
         self.parseRegionJson(responseObject)
         println("Region request job complete.")
@@ -115,7 +115,7 @@ public class EntityManager {
         self.createConnectionRequest()
         self.startBeaconRequests()
     }
-    
+
     private func handleBeaconRequestSuccess(operation: AFHTTPRequestOperation!, responseObject: AnyObject!, regionId:Int) {
         self.parseBeaconJson(responseObject, regionId: regionId)
         println("Beacon request job for region \(regionId) complete.")
@@ -125,22 +125,23 @@ public class EntityManager {
             self.connectionRequestOperation.start()
         }
     }
-    
+
     private func handleConnectionRequestSuccess(operation: AFHTTPRequestOperation!, responseObject: AnyObject!) {
         self.parseConnectionJson(responseObject)
         println("Connection request job complete.")
         loadingEntitiesDidComplete()
     }
-    
+
     private func loadingEntitiesDidComplete() {
         self.entitiesAreLoaded = true
         self.loadScreenController.loadingEntitiesDidComplete()
     }
-    
+
     private func handleRequestFailure(operation: AFHTTPRequestOperation!, error: NSError!) {
+        // TODO re-initiate request
         println("'\(operation.request.URL!.relativePath!)' fetch error: " + error.localizedDescription)
     }
-    
+
     private func parseRegionJson(jsonData: AnyObject) {
         let regionsJsonArray = JSON(jsonData)
         for (_, regionJson: JSON) in regionsJsonArray {
@@ -148,7 +149,7 @@ public class EntityManager {
             regions[region.regionId] = region
         }
     }
-    
+
     private func parseBeaconJson(jsonData: AnyObject, regionId:Int) {
         let beaconsJsonArray = JSON(jsonData)
         let region = getRegion(ID: regionId)
@@ -158,7 +159,7 @@ public class EntityManager {
             self.beaconsToRegions[beacon.beaconId] = regionId
         }
     }
-    
+
     private func parseConnectionJson(jsonData: AnyObject) {
         let connectionsJsonArray = JSON(jsonData)
         for (_, connectionJson: JSON) in connectionsJsonArray {
@@ -173,20 +174,30 @@ public class EntityManager {
             }
         }
     }
-    
+
     public func getRegion(ID id:Int) -> Region {
         return self.regions[id]!
     }
-    
+
     public func getRegionOfBeacon(beaconID id:Int) -> Region {
         let regionId = self.beaconsToRegions[id]!
         return self.getRegion(ID: regionId)
     }
-    
+
     public func getBeacon(ID id:Int) -> Beacon {
         return self.beacons[id]!
     }
-    
+
+    public func getBeaconOfConnectionWithID(connectionId:Int, atRegionWithID regionId:Int) -> Beacon! {
+        let connection = getConnection(ID: connectionId)
+        for beacon in connection.beacons {
+            if beacon.region.id == regionId {
+                return beacon
+            }
+        }
+        return nil
+    }
+
     public func getBeaconBy(#uuid: String, major: Int, minor: Int) -> Beacon? {
         let filteredBeacons = beacons.values.filter({
             (beacon:Beacon)->Bool in
@@ -198,21 +209,21 @@ public class EntityManager {
             return nil
         }
     }
-    
+
     public func getConnection(ID id:Int) -> Connection {
         return self.connections[id]!
     }
-    
+
     private func getJsonUrlRequest(#url: String) -> NSMutableURLRequest {
         let request = AFJSONRequestSerializer().requestWithMethod("POST", URLString: url, parameters: serverManager.getQueryAuthenticationJsonAsDict(), error: nil)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         return request
     }
-    
+
     public func getRegionsAsDisplayList() -> ([Int], [String]) {
         let regionIds:[Int] = self.regions.keys.array
         var regionTableItems:[String] = [String]()
-        
+
         for regionId in regionIds {
             let region = self.getRegion(ID: regionId)
             if let displayName = region.displayName {
@@ -221,7 +232,7 @@ public class EntityManager {
                 regionTableItems.append("- (\(region.id))")
             }
         }
-        
+
         return (regionIds, regionTableItems)
     }
 }
